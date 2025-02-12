aabb67ce119b3c39507a1c5c9f169ccd6d1c015a
parthiban-alt <ps4972083@gmail.com>
2025-02-12 18:58:58 +0530

    add

---
+++ version.php
@@ -0,0 +1,128 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Highshine</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css2?family=Archivo+Narrow:ital,wght@0,400..700;1,400..700&display=swap" rel="stylesheet">
+    <link href="version.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Archivo+Narrow:ital,wght@0,400..700;1,400..700&display=swap" rel="stylesheet">
+</head>
+<body>
+<nav class="navbar sticky-top navbar-expand-lg mt-5" id="navbar">
+    <div class="container">
+        <button class="navbar-toggler bg-white" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav w-100 d-flex justify-content-between mb-2 ">
+                <li class="nav-item">
+                    <a href="#" class="nav-link text-white">IFS Cloud</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link text-white">IFS ERP</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link text-white">IFS Enterprise Asset Management</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link text-white">IFS Field Service Management</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+
+<main>
+    <section>
+        <div class="container mt-5 mb-5" id="pills-tabContent">
+            <div class="tab-pane fade active show" id="pills-cloud" role="tabpanel" aria-labelledby="pills-cloud-tab">
+        <div class="row ">
+            <div class="col-lg-4 d-flex align-items-center">
+                <h1 class="content-1"style="font-weight:bold;">Why<br> <span class="color" style="color:#42348B;">IFS</span> <span>Cloud</span></h1> 
+            </div>
+            <div class="col-lg-8 d-flex align-items-justify text">
+            He IFS Cloud platform is a comprehensive suite of flexible ERP Solutions that empower businesses to optimize operations and drive revenue growth. Centralized via Cloud Processing, businesses can benefit from scalability, flexibility, and reduced IT costs. IFS Cloud provides a secure and reliable platform that can be accessed from anywhere, at any time.
+            </div>
+        </div>
+    <div class="row align-items-stretch ">
+        <div class="col-lg-4 d-flex flex-column mt-2 ">
+            <div class="card IFS ">Industry Specific Capabilities</div>
+            <div class="card IFS">Unified Platform</div>
+            <div class="card IFS">Intuitively Designed UI</div>
+            <div class="card IFS">Ever Evolving Functionality</div>
+            <div class="card IFS">Driven By Innovation</div>
+        </div>
+        <div class="col-lg-8 d-flex" >
+            <div class="row align-items-stretch">
+                <div class="col-lg-8 mt-md-2 d-flex" style=" background: linear-gradient(143.1deg, #42348B 24.41%, #A677C5 132.12%);padding:20px;border-radius:20px;color:white;  line-height: 1.3; ">
+                    <div class="container">
+                        <h2 class="content-1"style="font-size:40px;font-weight:bold;">A Forrester Report investigated</h2>
+                        <p class="text">The cost savings and business benefits enabled by IFS Solutions Deployed in the cloud in April 2023.</p>
+                        <p class="text">It found that the major areas in savings came from:</p>
+                        <ul class="text">
+                            <li>Labor Efficiencies Reallocated To <br>Growth Activities</li>
+                            <li>Capital Cost.</li>
+                            <li>Reporting And Uptime.</li>
+                            <li>Savings From Decommissioning <br>Legacy On-Prem Environment.</li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-lg-4 mt-md-2 d-flex">
+                    <div class="rlt_card  text-center w-100 d-flex flex-column justify-content-center"style=" background-image: linear-gradient(to bottom right, #42348B, #A677C5);padding:20px;border-radius:20px;color:white;">
+                        <h5>Access the full report below for more information:</h5>
+                        <a href="#" target="_blank" class="text-decoration-none">
+                            <div style="background-color: #EDDFFB;" class="rounded-pill px-3 py-2 d-inline-flex align-items-center justify-content-around">
+                                <h6 style="color: #845EC2;" class="mb-0">Download Pdf</h6>
+                                <img src="assets/download.png" width="25" alt="phone_img" class="img-fluid ms-3">
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+    <div class="row my-2 align-items-stretch d-flex ">
+        <div class="col-lg-6 mt-md-2">
+            <div class="card rounded-4" style="background: #F6F0FF;border:none;">
+                <div class="card-body">
+                    <h5 class="custom-heading text ps-2" >
+                    To know more on what a composable enterprises is and <br>how it compares to monolithic applications
+                    </h5>
+                    <a href="#" class="download-link text-decoration-none d-flex align-items-right justify-content-end" target="_blank">
+                    <div style="background-color:#845EC2;" class="rounded-pill px-3 py-1 d-inline-flex align-items-center justify-content-around">
+                                <h6 style="color:white" class="mb-0">Download Pdf</h6>
+                                <img src="assets/white-download.png" width="25" alt="phone_img" class="img-fluid ms-3">
+                            </div>
+                    </a>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-lg-6 mt-md-2">
+            <div class="card rounded-4" style="background-color:#F2F6FF;border:none">
+                <div class="card-body">
+                    <h5 class="custom-heading text ps-2">
+                    To know more on how IFS Cloud supplement<br> sustainable growth
+                    </h5>
+                    <!-- <a href="#" class="download-link text-decoration-none d-flex align-items-right" target="_blank"> -->
+                    <a href="#" class="download-link text-decoration-none d-flex align-items-right justify-content-end" target="_blank">
+                    <div style="background-color:#845EC2;" class="rounded-pill px-3 py-1 d-inline-flex align-items-center justify-content-around">
+                                <h6 style="color:white;" class="mb-0">Download Pdf</h6>
+                                <img src="assets/white-download.png" width="25" alt="phone_img" class="img-fluid ms-3">
+                            </div>
+                    </a>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</div>
+</div>
+    </section>
+</main>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
